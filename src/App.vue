9f45f5673471abbc2bf9c6542b28807cfb218740
nagisa yamazaki <sky.nk.630@gmail.com>
2021-01-17 15:44:44 +0900

    change src

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <!-- TODO inputで入力した際に入力した内容を受け取れる様にv-modelを使用する -->
+     <input type="text" v-model="text" />
+     <!-- TODO クリックイベントを作成してリクエストを送信する関数を実行 -->
+  <button v-on:click="userClick">住所自動入力</button>
+<!-- TODO マスタッシュ記法で住所が代入されたへ変数を描画 -->
+  <p>Address:{{ text }}</p>
+  </div>
+</template>
+
+<script>
+  import axios from "axios";
+export default {
+  data() {
+    return{
+      text: "",
+    }
+  },
+}
+</script>
